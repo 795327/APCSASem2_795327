62d964a024472e8306af994c03d25560f9171517
795327 <795327@seq.org>
2019-01-15 23:16:33 -0800

    comments made in Lab 114b

--- Labs/Lab 114b SortingArrayList/SortingArrayListRunner.java
+++ Labs/Lab 114b SortingArrayList/SortingArrayListRunner.java
@@ -9,124 +9,201 @@ import java.util.concurrent.TimeUnit;
 import java.util.ArrayList;
 public class SortingArrayListRunner{
     public static void main(){
+        // declaring and initializing two ArrayLists; 
+        // one for original ints, one for sorted ints
         ArrayList<Integer> nums = new ArrayList<Integer>();
         ArrayList<Integer> sortedNums = new ArrayList<Integer>();
-
+        
+        // filling nums ArrayList with Integers
         nums.add(1); nums.add(5); nums.add(3); nums.add(7); nums.add(2);
-
+        
+        // calls for each sorting method (uncomment one and run to use)
         // sortedNums = mySort(nums);
         // sortedNums = bubbleSort(nums);
-        sortedNums = selectionSort(nums);
+        // sortedNums = selectionSort(nums);
         // sortedNums = insertionSort(nums);
-
+        
+        // prints sorted nums ArrayList
         System.out.println(sortedNums);
     }
-
-    public static ArrayList<Integer> mySort(ArrayList<Integer> nonSortedArray){
+    
+    // my own sorting method, identical to bubble sorting algorithm
+    public static ArrayList<Integer> mySort(ArrayList<Integer> nonSortedArrayList){
+        // starts timer in nanoseconds to calculate execution time for method
         double startTime = System.nanoTime();
+        
+        // declares and initializes comparisons and swaps variables used to count times such interactions are made
         int comparisons = 0;
         int swaps = 0;
-        ArrayList<Integer> sortedArray = new ArrayList<Integer>();
+        
+        // declares and inits an ArrayList to store the sorted ints
+        ArrayList<Integer> sortedArrayList = new ArrayList<Integer>();
+        
+        // declared to keep an int from being lost during a swap
         int currentNum;
-        for (int i = 0; i < nonSortedArray.size() - 1; i++){
-            for (int j = i + 1; j < nonSortedArray.size(); j++){
+        
+        // for loop that traverses and sorts the nonSortedArrayList
+        for (int i = 0; i < nonSortedArrayList.size() - 1; i++){
+            for (int j = i + 1; j < nonSortedArrayList.size(); j++){
                 comparisons++;
-                if (nonSortedArray.get(i) > nonSortedArray.get(j)){
-                    currentNum = nonSortedArray.get(i);
-                    nonSortedArray.set(i, nonSortedArray.get(j));
-                    nonSortedArray.set(j, currentNum);
+                if (nonSortedArrayList.get(i) > nonSortedArrayList.get(j)){
+                    currentNum = nonSortedArrayList.get(i);
+                    nonSortedArrayList.set(i, nonSortedArrayList.get(j));
+                    nonSortedArrayList.set(j, currentNum);
                     swaps++;
                 }
             }
         }
-        sortedArray = nonSortedArray;
+        sortedArrayList = nonSortedArrayList;
+        
+        // ends timer
         double endTime = System.nanoTime();
+        
+        // calculates method execution time
         double timeElapsed = endTime - startTime;
+        
+        // converts nanosecond time to miliseconds and prints it to the console
         System.out.println("Execution time in milliseconds: " + 
             timeElapsed / 1000000);
+        
+        // prints number of swaps and comparisons to console
         System.out.println("Swaps: " + swaps); 
         System.out.println("Comparisons: " + comparisons);
-        return sortedArray;
+        
+        return sortedArrayList;
     }
-
-    public static ArrayList<Integer> bubbleSort(ArrayList<Integer> nonSortedArray){
+    
+    // method for bubble sorting algorithm
+    public static ArrayList<Integer> bubbleSort(ArrayList<Integer> nonSortedArrayList){
+        // starts timer in nanoseconds to calculate execution time for method
         double startTime = System.nanoTime();
+        
+        // declares and initializes comparisons and swaps variables used to count times such interactions are made
         int comparisons = 0;
         int swaps = 0;
-        ArrayList<Integer> sortedArray = new ArrayList<Integer>();
-        for (int i = 0; i < nonSortedArray.size() - 1; i++){
-            for (int j = i + 1; j < nonSortedArray.size(); j++){
+        
+        // declares and inits an ArrayList to store the sorted ints
+        ArrayList<Integer> sortedArrayList = new ArrayList<Integer>();
+        
+        // for loop that traverses and sorts the nonSortedArrayList
+        for (int i = 0; i < nonSortedArrayList.size() - 1; i++){
+            for (int j = i + 1; j < nonSortedArrayList.size(); j++){
                 comparisons++;
-                if (nonSortedArray.get(i) > nonSortedArray.get(j)){
-                    int temp = nonSortedArray.get(i);
-                    nonSortedArray.set(i, nonSortedArray.get(j));
-                    nonSortedArray.set(j, temp);
+                if (nonSortedArrayList.get(i) > nonSortedArrayList.get(j)){
+                    int temp = nonSortedArrayList.get(i);
+                    nonSortedArrayList.set(i, nonSortedArrayList.get(j));
+                    nonSortedArrayList.set(j, temp);
                     swaps++;
                 }
             }
         }
-        sortedArray = nonSortedArray;
+        sortedArrayList = nonSortedArrayList;
+        
+        // ends timer
         double endTime = System.nanoTime();
+        
+        // calculates method execution time
         double timeElapsed = endTime - startTime;
+        
+        // converts nanosecond time to miliseconds and prints it to the console
         System.out.println("Execution time in milliseconds: " + 
             timeElapsed / 1000000);
+            
+        // prints number of swaps and comparisons to console
         System.out.println("Swaps: " + swaps); 
         System.out.println("Comparisons: " + comparisons);
-        return sortedArray;
+        
+        return sortedArrayList;
     }
-
-    public static ArrayList<Integer> selectionSort(ArrayList<Integer> nonSortedArray){
+    
+    // method for selection sort algorithm
+    public static ArrayList<Integer> selectionSort(ArrayList<Integer> nonSortedArrayList){
+        // starts timer in nanoseconds to calculate execution time for method
         double startTime = System.nanoTime();
+        
+        // declares and initializes comparisons and swaps variables used to count times such interactions are made
         int comparisons = 0;
         int swaps = 0;
-        ArrayList<Integer> sortedArray = new ArrayList<Integer>();
+        
+        // declares and inits an ArrayList to store the sorted ints
+        ArrayList<Integer> sortedArrayList = new ArrayList<Integer>();
+        
+        // int declared to keep track of the current Integer being used from the nonSortedArrayList
         int index;
-        for (int i = 0; i < nonSortedArray.size() - 1; i++){
+        
+        // for loop that traverses and sorts the nonSortedArrayList
+        for (int i = 0; i < nonSortedArrayList.size() - 1; i++){
             index = i;
-            for (int j = i + 1; j < nonSortedArray.size(); j++){
+            for (int j = i + 1; j < nonSortedArrayList.size(); j++){
                 comparisons++;
-                if (nonSortedArray.get(j) < nonSortedArray.get(index)){
+                if (nonSortedArrayList.get(j) < nonSortedArrayList.get(index)){
                     index = j;
                 }
             }
-            int temp = nonSortedArray.get(index);
-            nonSortedArray.set(index, nonSortedArray.get(i));
-            nonSortedArray.set(i, temp);
+            int temp = nonSortedArrayList.get(index);
+            nonSortedArrayList.set(index, nonSortedArrayList.get(i));
+            nonSortedArrayList.set(i, temp);
             swaps++;
         }
-        sortedArray = nonSortedArray;
+        sortedArrayList = nonSortedArrayList;
+        
+        // ends timer
         double endTime = System.nanoTime();
+        
+        // calculates method execution time
         double timeElapsed = endTime - startTime;
+        
+        // converts nanosecond time to miliseconds and prints it to the console
         System.out.println("Execution time in milliseconds: " + 
             timeElapsed / 1000000);
+            
+        // prints number of swaps and comparisons to console
         System.out.println("Swaps: " + swaps); 
         System.out.println("Comparisons: " + comparisons);
-        return sortedArray;
+        
+        return sortedArrayList;
     }
-
-    public static ArrayList<Integer> insertionSort(ArrayList<Integer> nonSortedArray){
+    
+    // method for insertion sort algorithm
+    public static ArrayList<Integer> insertionSort(ArrayList<Integer> nonSortedArrayList){
+        // starts timer in nanoseconds to calculate execution time for method
         double startTime = System.nanoTime();
+        
+        // declares and initializes comparisons and swaps variables used to count times such interactions are made
         int comparisons = 0;
         int swaps = 0;
-        ArrayList<Integer> sortedArray = new ArrayList<Integer>();
-        for (int i = 1; i < nonSortedArray.size(); i++){
+        
+        // declares and inits an ArrayList to store the sorted ints
+        ArrayList<Integer> sortedArrayList = new ArrayList<Integer>();
+        
+        // for loop that traverses and sorts the nonSortedArrayList
+        for (int i = 1; i < nonSortedArrayList.size(); i++){
             for (int j = i; j > 0; j--){
                 comparisons++;
-                if (nonSortedArray.get(j) < nonSortedArray.get(j - 1)){
-                    int temp = nonSortedArray.get(j);
-                    nonSortedArray.set(j, nonSortedArray.get(j - 1));
-                    nonSortedArray.set(j - 1, temp);
+                if (nonSortedArrayList.get(j) < nonSortedArrayList.get(j - 1)){
+                    int temp = nonSortedArrayList.get(j);
+                    nonSortedArrayList.set(j, nonSortedArrayList.get(j - 1));
+                    nonSortedArrayList.set(j - 1, temp);
                     swaps++;
                 }
             }
         }
-        sortedArray = nonSortedArray;
+        sortedArrayList = nonSortedArrayList;
+        
+        // ends timer
         double endTime = System.nanoTime();
+        
+        // calculates method execution time
         double timeElapsed = endTime - startTime;
+        
+        // converts nanosecond time to miliseconds and prints it to the console
         System.out.println("Execution time in milliseconds: " + 
             timeElapsed / 1000000);
+            
+        // prints number of swaps and comparisons to console
         System.out.println("Swaps: " + swaps); 
         System.out.println("Comparisons: " + comparisons);
-        return sortedArray;
+        
+        return sortedArrayList;
     }
 }
