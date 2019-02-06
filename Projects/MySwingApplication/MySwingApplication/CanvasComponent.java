53c0a8037281295a02802937b98f1cb6fbf805df
795327 <795327@seq.org>
2019-02-06 10:20:22 -0800

    working on Activity 3

--- Projects/MySwingApplication/MySwingApplication/CanvasComponent.java
+++ Projects/MySwingApplication/MySwingApplication/CanvasComponent.java
@@ -7,23 +7,65 @@
  */
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 public class CanvasComponent extends JComponent
 {
     int rX;
     int rY;
     int width;
     int height;
+    int mouseFromX;
+    int mouseFromY;
+    int mouseToX;
+    int mouseToY;
+    boolean shapeSelected;
     
-    public CanvasComponent(int width, int height){
+    public CanvasComponent(int w, int h){
+        setSize(w, h);
         rX = 0;
         rY = 0;
-        width = 100;
-        height = 100;
-        setSize(width, height);
+        width = 200;
+        height = 200;
     }
     
-    protected void paintComponent(Graphics g){
-        g.setColor(Color.blue);
+    public void paintComponent(Graphics g){
+        g.setColor(Color.cyan);
         g.fillRect(rX, rY, width, height);
+    }
+    
+    public void mouseClicked(){
+        
+    }
+    
+    public void mousePressed(MouseEvent e){
+        mouseFromX = e.getX();
+        mouseFromY = e.getY();
+        
+        if (shapeSelected){
+            
+        }
+    }
+    
+    public void mouseReleased(){
+        
+    }
+    
+    public void mouseEntered(){
+        
+    }
+    
+    public void mouseExited(){
+        
+    }
+    
+    public void mouseDragged(MouseEvent e){
+        mouseToX = e.getX();
+        mouseToY = e.getY();
+        rX += (mouseToX - mouseFromX);
+        rY += (mouseToY - mouseFromY);
+    }
+    
+    public void mouseMoved(){
+        
     }
 }
--- Projects/MySwingApplication/MySwingApplication/MySwingApplication.java
+++ Projects/MySwingApplication/MySwingApplication/MySwingApplication.java
@@ -11,19 +11,17 @@ public class MySwingApplication implements Runnable
     private JFrame jFrame;
     private CanvasComponent canvasComponent;
     
-    public static void main() {
-        MySwingApplication mySwingApplicationSwing = new MySwingApplication();
-        javax.swing.SwingUtilities.invokeLater(mySwingApplicationSwing);
-    }
-    
     public void run(){
         jFrame = new JFrame("Magic Window");
         jFrame.setSize(400, 400);
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        canvasComponent = new CanvasComponent(200, 200);
-        canvasComponent.setSize(200, 200);
-        // paint component?
+        canvasComponent = new CanvasComponent(100, 100);
         jFrame.add(canvasComponent);
         jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    public static void main() {
+        MySwingApplication mySwingApplicationSwing = new MySwingApplication();
+        javax.swing.SwingUtilities.invokeLater(mySwingApplicationSwing);
     }
 }
