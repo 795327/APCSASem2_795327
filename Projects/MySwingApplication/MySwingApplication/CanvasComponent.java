65b151d74301af6acccd6c10a0fedd884120ba07
795327 <795327@seq.org>
2019-02-08 10:30:50 -0800

    finished activity 3

--- Projects/MySwingApplication/MySwingApplication/CanvasComponent.java
+++ Projects/MySwingApplication/MySwingApplication/CanvasComponent.java
@@ -8,7 +8,7 @@
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
-public class CanvasComponent extends JComponent
+public class CanvasComponent extends JComponent implements MouseListener, MouseMotionListener, ActionListener
 {
     int rX;
     int rY;
@@ -19,53 +19,80 @@ public class CanvasComponent extends JComponent
     int mouseToX;
     int mouseToY;
     boolean shapeSelected;
-    
+    int animationDeltaX;
+    int animationDeltaY;
+    int gutterX;
+    int gutterY;
+
     public CanvasComponent(int w, int h){
         setSize(w, h);
-        rX = 0;
-        rY = 0;
-        width = 200;
-        height = 200;
+        rX = 150;
+        rY = 150;
+        width = 100;
+        height = 100;
+        this.addMouseListener(this);
+        this.addMouseMotionListener(this);
+        animationDeltaX = 1;
+        animationDeltaY = 0;
+        gutterX = 10;
+        gutterY = 10;
     }
-    
+
     public void paintComponent(Graphics g){
         g.setColor(Color.cyan);
         g.fillRect(rX, rY, width, height);
     }
-    
-    public void mouseClicked(){
-        
-    }
-    
+
     public void mousePressed(MouseEvent e){
         mouseFromX = e.getX();
         mouseFromY = e.getY();
-        
-        if (shapeSelected){
-            
+        if (mouseFromX > rX && mouseFromY > rY && rX + width >= mouseFromX && rY + height >= mouseFromY){
+            shapeSelected = true;
+        } else {shapeSelected = false;}
+    }
+
+    public void mouseDragged(MouseEvent e){
+        if (shapeSelected == true){
+            mouseToX = e.getX();
+            mouseToY = e.getY();
+            rX += (mouseToX - mouseFromX);
+            rY += (mouseToY - mouseFromY);
+            mouseFromX = mouseToX;
+            mouseFromY = mouseToY;
+            repaint();
         }
     }
     
-    public void mouseReleased(){
-        
+    public void actionPerformed(ActionEvent e){
+        if (shapeSelected == false){
+            Dimension componentSizeDimension = new Dimension(getSize());
+            if (rX + width + gutterX > componentSizeDimension.getWidth()){
+                rX = ((int)componentSizeDimension.getWidth() - width - gutterX);
+                rY += animationDeltaY;
+            }
+            if (rY + height + gutterY > 400){
+                
+            }
+        }
     }
-    
-    public void mouseEntered(){
-        
+
+    public void mouseClicked(MouseEvent e){
+
     }
-    
-    public void mouseExited(){
-        
+
+    public void mouseReleased(MouseEvent e){
+
     }
-    
-    public void mouseDragged(MouseEvent e){
-        mouseToX = e.getX();
-        mouseToY = e.getY();
-        rX += (mouseToX - mouseFromX);
-        rY += (mouseToY - mouseFromY);
+
+    public void mouseEntered(MouseEvent e){
+
     }
-    
-    public void mouseMoved(){
-        
+
+    public void mouseExited(MouseEvent e){
+
+    }
+
+    public void mouseMoved(MouseEvent e){
+
     }
 }
