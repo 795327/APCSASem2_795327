68c2e24268ce2f77f5f29691228958b68b70cca1
795327 <795327@seq.org>
2019-02-14 10:30:40 -0800

    Elevens - activity 1 complete

---
+++ Projects/Project 6 Elevens/Activity1 Starter Code/CardTester.java
@@ -0,0 +1,23 @@
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+    /**
+     * The main method in this class checks the Card operations for consistency.
+     *	@param args is not used.
+     */
+    public static void main(String[] args) {
+        Card a = new Card("A", "clubs", 1);
+        Card b = new Card("4", "diamonds", 4);
+        Card c = new Card("4", "diamonds", 4);
+        // System.out.println(b.matches(c));
+        // System.out.println(b.matches(a));
+        // System.out.println(c.matches(a));
+        // System.out.println(b.suit());
+        // System.out.println(b.rank());
+        // System.out.println(c.pointValue());
+        // System.out.println(b.toString());
+        // System.out.println(a.toString());
+    }
+}
